7b4d8a8d9e77693344e19019cc0b7e6752bb3554
Naveen <naveen80567@gmail.com>
2024-11-06 20:29:17 +0530

    deploy

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-PULIETLU.mjs";import"./chunk-RYHOZRBQ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/custom-sw.js')
+    navigator.serviceWorker.register('/app/custom-sw.js')
       .then((registration) => {
         console.log('Custom Service Worker registered with scope:', registration.scope);
       })
